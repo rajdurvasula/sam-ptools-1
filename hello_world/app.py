dd34864fde7e787e8802d650dbf16e26af2e1c06
rdurvas1 <rajasekhar.durvasula@ocean.ibm.com>
2022-10-30 06:05:48 +0000

    simplify custom metrics

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,56 +1,21 @@
-import os
+
 import json
-import boto3
-from aws_xray_sdk.core import xray_recorder
-from aws_lambda_powertools import Logger, Tracer
-from pythonjsonlogger import jsonlogger
+from aws_lambda_powertools import Logger, Tracer, Metrics
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
-
-
-cold_start = True
-metric_namespace = 'MyApp'
+from aws_lambda_powertools.metrics import MetricUnit
 
 logger = Logger(service="APP")
 tracer = Tracer(service="APP")
-metrics = boto3.client('cloudwatch')
+metrics = Metrics(namespace='MyApp', service='APP')
 app = APIGatewayRestResolver()
-
-@tracer.capture_method
-def add_greeting_metric(service: str = 'APP'):
-    function_name = os.getenv('AWS_LAMBDA_FUNCTION_NAME', 'undefined')
-    service_dimension = {
-        'Name': 'Service',
-        'Value': service
-    }
-    function_dimension = {
-        'Name': 'function_name',
-        'Value': function_name
-    }
-    is_cold_start = True
-    return metrics.put_metric_data(
-        MetricData = [
-            {
-                'MetricName': 'SuccessfullGreeting',
-                'Dimensions': [ service_dimension ],
-                'Unit': 'Count',
-                'Value': 1
-            },
-            {
-                'MetricName': 'ColdStart',
-                'Dimensions': [ service_dimension, function_dimension ],
-                'Unit': 'Count',
-                'Value': int(is_cold_start)
-            }
-        ],
-        Namespace=metric_namespace)
 
 @app.get('/hello/<name>')
 @tracer.capture_method
 def hello_name(name):
     tracer.put_annotation(key='User', value=name)
     logger.info(f'Request from {name} received.')
-    add_greeting_metric()
+    metrics.add_metric(name='SuccessfulGreetings', unit=MetricUnit.Count, value=1)
     return {
         'message': f'Hello {name} !'
     }
@@ -60,13 +25,14 @@ def hello_name(name):
 def hello():
     tracer.put_annotation(key='User', value='unknown')
     logger.info('Request from Unknown received.')
-    add_greeting_metric()
+    metrics.add_metric(name='SuccessfulGreetings', unit=MetricUnit.Count, value=1)
     return {
         'message': 'hello unknown !'
     }
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
 @tracer.capture_lambda_handler
+@metrics.log_metrics(capture_cold_start_metric=True)
 def lambda_handler(event, context):
     """Sample pure Lambda function
 
@@ -104,4 +70,8 @@ def lambda_handler(event, context):
     #        # "location": ip.text.replace("\n", "")
     #    }),
     #}
-    return app.resolve(event, context)
+    try:
+        return app.resolve(event, context)
+    except Exception as e:
+        logger.exception(e)
+        raise

895753f453b62b74f4172308b4f9d3088b07ad78
rdurvas1 <rajasekhar.durvasula@ocean.ibm.com>
2022-10-30 05:27:38 +0000

    simplify tracing with tracer

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,32 +1,36 @@
 import json
 from aws_xray_sdk.core import xray_recorder
-from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Logger, Tracer
 from pythonjsonlogger import jsonlogger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 
 logger = Logger(service="APP")
-
+tracer = Tracer(service="APP")
 app = APIGatewayRestResolver()
+cold_start = True
+
 
 @app.get('/hello/<name>')
-@xray_recorder.capture('hello_name')
+@tracer.capture_method
 def hello_name(name):
+    tracer.put_annotation(key='User', value=name)
     logger.info(f'Request from {name} received.')
     return {
         'message': f'Hello {name} !'
     }
 
 @app.get('/hello')
-@xray_recorder.capture('hello')
+@tracer.capture_method
 def hello():
+    tracer.put_annotation(key='User', value='unknown')
     logger.info('Request from Unknown received.')
     return {
         'message': 'hello unknown !'
     }
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
-@xray_recorder.capture('handler')
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
     """Sample pure Lambda function
 

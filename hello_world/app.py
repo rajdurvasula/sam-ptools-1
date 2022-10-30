0e0762b2a24692d237fd2e8914c4898199ff2df0
rdurvas1 <rajasekhar.durvasula@ocean.ibm.com>
2022-10-30 05:56:14 +0000

    use custom metrics

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,21 +1,56 @@
+import os
 import json
+import boto3
 from aws_xray_sdk.core import xray_recorder
 from aws_lambda_powertools import Logger, Tracer
 from pythonjsonlogger import jsonlogger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 
+
+cold_start = True
+metric_namespace = 'MyApp'
+
 logger = Logger(service="APP")
 tracer = Tracer(service="APP")
+metrics = boto3.client('cloudwatch')
 app = APIGatewayRestResolver()
-cold_start = True
 
+@tracer.capture_method
+def add_greeting_metric(service: str = 'APP'):
+    function_name = os.getenv('AWS_LAMBDA_FUNCTION_NAME', 'undefined')
+    service_dimension = {
+        'Name': 'Service',
+        'Value': service
+    }
+    function_dimension = {
+        'Name': 'function_name',
+        'Value': function_name
+    }
+    is_cold_start = True
+    return metrics.put_metric_data(
+        MetricData = [
+            {
+                'MetricName': 'SuccessfullGreeting',
+                'Dimensions': [ service_dimension ],
+                'Unit': 'Count',
+                'Value': 1
+            },
+            {
+                'MetricName': 'ColdStart',
+                'Dimensions': [ service_dimension, function_dimension ],
+                'Unit': 'Count',
+                'Value': int(is_cold_start)
+            }
+        ],
+        Namespace=metric_namespace)
 
 @app.get('/hello/<name>')
 @tracer.capture_method
 def hello_name(name):
     tracer.put_annotation(key='User', value=name)
     logger.info(f'Request from {name} received.')
+    add_greeting_metric()
     return {
         'message': f'Hello {name} !'
     }
@@ -25,6 +60,7 @@ def hello_name(name):
 def hello():
     tracer.put_annotation(key='User', value='unknown')
     logger.info('Request from Unknown received.')
+    add_greeting_metric()
     return {
         'message': 'hello unknown !'
     }
